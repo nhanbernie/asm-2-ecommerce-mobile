dcf6c16c7aab9dcb3771e728cc1d646bd0d4138a
nhanbernie <ttnhanbernie@gmail.com>
2025-07-21 10:37:51 +0700

    hide:  error log not important on ui

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import AppProvider from "@/providers/AppProvider";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "./global.css";
-
+import { LogBox } from "react-native";
+LogBox.ignoreAllLogs(true);
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
